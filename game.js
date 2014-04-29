090f26c7d8a874eeb7e80bade4a1bde2bb4c888f
mordrax <joseph.ni@gmail.com>
2014-04-29 23:24:24 +1000

    added constructs for world dimension and data

--- game.js
+++ game.js
@@ -5,6 +5,16 @@ var fs = require('fs');
 
 var world = JSON.parse(fs.readFileSync('world'));
 
+// the x and y dimension of the world
+world.dimension = world.dimension || {x:100, y:100};
+
+// the array that contains the ground layer of the world
+world.data = world.data || new Array(world.dimension.x);
+for (var i in world.data) {
+    world.data[i] = new Array(world.dimension.y);
+}
+
+
 var run = function() {
     // world evolving
     world.time = (new Date()).toString();
